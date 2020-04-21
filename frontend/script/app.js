7609980e17a9e085092fa71dfad5db808846ecfc
Simon Van Den Hende <simonvdhende@outlook.com>
2020-04-21 15:59:01 +0200

    first styling pass

--- frontend/script/app.js
+++ frontend/script/app.js
@@ -4,57 +4,89 @@
 const size = 13;
 let htmlBoard;
 let highlightedCell;
+let wordsFound = 0;
+
+const crossOutWord = function (word) {
+    for (const w of document.querySelectorAll(".wordListItem")) {
+        if (word === w.innerHTML) {
+            w.classList.add("crossed");
+            break;
+        }
+    }
+};
+
+const updateProgressBar = function(amountOfWords) {
+        // update progressbar
+        const percentage = `${Math.round(wordsFound / amountOfWords * 100)}%`;
+        document.querySelector('.bar').style.width = percentage;
+        document.querySelector('.bar').innerHTML = percentage;
+}
 
 const addEventsToBoard = function (words) {
-    console.log(words);
-
     for (const cell of document.querySelectorAll("td")) {
         cell.addEventListener("click", function () {
             if (highlightedCell != null) {
                 // Clicked for 2nd time -> lookup word
-                highlightedCell.classList.remove('highlight');
+                highlightedCell.classList.remove("highlight");
 
-                const firstLetterX = highlightedCell.getAttribute('data-x');
-                const firstLetterY = highlightedCell.getAttribute('data-y');
+                // Store first letter coordinates in the right format
+                const firstLetterX = highlightedCell.getAttribute("data-x");
+                const firstLetterY = highlightedCell.getAttribute("data-y");
                 const firstLetterCoords = `${firstLetterX}-${firstLetterY}`;
-                console.log(firstLetterCoords);
-                
-                const lastLetterX = this.getAttribute('data-x');
-                const lastLetterY = this.getAttribute('data-y');
+
+                // Store last letter coordinates in the right format
+                const lastLetterX = this.getAttribute("data-x");
+                const lastLetterY = this.getAttribute("data-y");
                 const lastLetterCoords = `${lastLetterX}-${lastLetterY}`;
 
+                // Check whether the selected word is in the list of words
                 if (words.hasOwnProperty(firstLetterCoords)) {
-                    if (words[firstLetterCoords].last_letter == lastLetterCoords) {
-                        console.log(words[firstLetterCoords].word);
-                        for(const cell of words[firstLetterCoords].all_letters) {
-                            const cellX = cell.split('-')[0];
-                            const cellY = cell.split('-')[1];
-                            document.getElementById("playingBoard").rows[cellX].cells[cellY].classList.add('found');
+                    if (
+                        words[firstLetterCoords].last_letter == lastLetterCoords
+                    ) {
+                        // A word has been found
+                        wordsFound += 1;
+
+                        // Colorize the letters of the word that was found
+                        for (const cell of words[firstLetterCoords]
+                            .all_letters) {
+                            // Get coordinates of each cell of the word
+                            const cellX = cell.split("-")[0];
+                            const cellY = cell.split("-")[1];
+                            // Apply the style
+                            document
+                                .getElementById("playingBoard")
+                                .rows[cellX].cells[cellY].classList.add(
+                                    "found"
+                                );
+                            // Cross out the word
+                            crossOutWord(words[firstLetterCoords].word);
+                            // Update progressbar
+                            updateProgressBar(Object.keys(words).length);
                         }
                     }
                 }
-
+                // Turn the highlighted cell off again
                 highlightedCell = null;
-            }   else {
+            } else {
                 highlightedCell = this;
                 // Clicked first time -> highlight current cell
-                this.classList.add('highlight');
+                this.classList.add("highlight");
             }
         });
     }
 };
 
-const showWordSearch = function (jsonObject) {
-    console.log("Showing word search");
-
+const createTable = function (puzzle) {
+    // Generate html to form table
     let htmlString = '<table id="playingBoard">';
     for (let x = 0; x < size; x++) {
         htmlString += `<tr>`;
         for (let y = 0; y < size; y++) {
             let letter = "-";
             const key = `${x}-${y}`;
-            if (key in jsonObject.puzzle) {
-                letter = jsonObject.puzzle[key];
+            if (key in puzzle) {
+                letter = puzzle[key];
             }
             htmlString += `<td data-x="${x}" data-y="${y}">${letter.toUpperCase()}</td>`;
         }
@@ -62,12 +94,27 @@ const showWordSearch = function (jsonObject) {
     }
     htmlString += "</table>";
 
-    htmlBoard.innerHTML = htmlString;
+    // Put that table in the right div
+    document.querySelector(".js-board").innerHTML = htmlString;
+};
+
+const createWordList = function (words) {
+    let htmlString = "<ul>";
+    for (const key of Object.keys(words)) {
+        htmlString += `<li class="wordListItem">${words[key].word}</li>`;
+    }
+    htmlString += "</ul>";
+
+    document.querySelector(".js-words").innerHTML = htmlString;
+};
+
+const showWordSearch = function (jsonObject) {
+    createTable(jsonObject.puzzle);
+    createWordList(jsonObject.words);
     addEventsToBoard(jsonObject.words);
 };
 
 const getWordSearch = function () {
-    console.log("Getting word search");
     handleData(
         `http://127.0.0.1:5000/puzzle/${size}`,
         showWordSearch,
@@ -80,11 +127,25 @@ const callbackError = function (e) {
     console.log(e);
 };
 
+const setDimensions = function() {
+    const htmlRow = document.querySelector(".o-row");
+    let dimension = Math.min(htmlRow.scrollWidth, htmlRow.scrollHeight);
+
+    if (dimension === htmlRow.scrollWidth) {
+        // width < height (= portrait mode)
+        dimension *= 0.8;
+    } else {
+        // height < width (= landscape)
+        dimension *= 0.6;
+    }
+
+    htmlRow.style.setProperty("width", `${dimension}px`);
+    htmlRow.style.setProperty("height", `${dimension}px`);
+}
+
 /* init */
 const init = function () {
-    console.log("DOM Content Loaded");
-    htmlBoard = document.querySelector(".js-board");
-
+    setDimensions();
     getWordSearch();
 };
 
